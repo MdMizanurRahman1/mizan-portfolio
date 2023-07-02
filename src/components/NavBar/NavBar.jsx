2bd9199c96fb8dfd27452d6c64763d7fb7b18d90
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-07-02 13:48:03 +0300

    icons added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RiHome2Line, RiUserLine, RiBriefcaseLine, RiCodeLine, RiMailLine } from 'react-icons/ri';
 import './NavBar.css';
 
 const NavBar = () => {
+    const [activeNav, setActiveNav] = useState('#')
+
     return (
-        <nav className="navbar">
+        <nav className="navbar md:w-auto">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <a href="#header" className="nav-link">
+                    <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>
                         <RiHome2Line className="nav-icon" />
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a href="#aboutMe" className="nav-link">
+                    <a href="#aboutMe" onClick={() => setActiveNav('#aboutMe')} className={activeNav === '#aboutMe' ? 'active' : ''} >
                         <RiUserLine className="nav-icon" />
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a href="#skills" className="nav-link">
+                    <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}  >
                         <RiBriefcaseLine className="nav-icon" />
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a href="#projects" className="nav-link">
+                    <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}  >
                         <RiCodeLine className="nav-icon" />
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a href="#contact" className="nav-link">
+                    <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}  >
                         <RiMailLine className="nav-icon" />
                     </a>
                 </li>
             </ul>
-        </nav>
+        </nav >
     );
 };
 
